62c5375fa62d801873a338489aea379ecd9197ab
franzgerbig <franz.gerbig@posteo.de>
2023-06-30 16:58:51 +0200

    adapted to right data source
    source:
    https://www.kaggle.com/datasets/yashkantharia/kickstarter-campaigns-dataset-20

--- kaggle.py
+++ kaggle.py
@@ -20,14 +20,14 @@ sns.set() # to change the theme
 pd.options.display.max_columns=None
 
 # read data from downloaded file
-file=r"C:\Users\Franz.000\Documents\Berufliches\Weiterbildung\data analyst\data\kickstarter\kaggle\ks-projects-201801.csv"
+file=r"C:\Users\Franz.000\Documents\GitHub\MAY23_BDA_INT_Crowdfunding\data\kaggle\Kaggle_Dataset.csv"
 raw=pd.read_csv(file)
 raw.head(3)
 df=raw
 
-raw["launched_year"]=pd.to_datetime(raw["launched"])
-raw["year"]=raw["launched_year"].dt.year
-raw["year"].value_counts()
+# raw["launched_year"]=pd.to_datetime(raw["launched_at"])
+# raw["year"]=raw["launched_year"].dt.year
+# raw["year"].value_counts()
 
 # ------------------------------------------------------------------------------
 
@@ -38,15 +38,22 @@ raw["year"].value_counts()
 # df information
 df.info() # 378661 projects
 
-# What categories does the success indicator "state" have?
-df.state.value_counts(normalize=True)
-sns.countplot(data=df,x="state")
-plt.title("Frequencies of state categories")
+# detect (and delete duplicates)
+df.duplicated().sum() # 0
+df.id.duplicated().sum() # 22,170
+df.drop_duplicates(keep='first',inplace=True,subset='id')
+df.id.duplicated().sum() #
+len(df) # 192,888 remain
+
+# What categories does the success indicator "status" have?
+df.status.value_counts(normalize=True)
+sns.countplot(data=df,x="status")
+plt.title("Frequencies of status categories");
 
 # reduce to successful and failed projects, (finished projects) only
-df=df[(df.state=="successful")|(df.state=="failed")]
-len(df) # 331675 are relevant and retained
-df.state.value_counts(normalize=True) 
+df=df[(df.status=="successful")|(df.status=="failed")]
+len(df) # 180675 are relevant and retained (after dropping duplicates)
+df.status.value_counts(normalize=True) 
 
 
 ## date/time related information
@@ -54,7 +61,7 @@ df.state.value_counts(normalize=True)
 # derive year, month, week, and day (of the week) 
 from datetime import timedelta
 for p in ["year","month","week","day"]:
-    for v,var in zip(["launch","end"],["launched","deadline"]):
+    for v,var in zip(["launched","dead"],["launched_at","deadline"]):
         name=v+"_"+p
         # convert into datetime
         df[v]=pd.to_datetime(df[var])
@@ -62,29 +69,51 @@ for p in ["year","month","week","day"]:
         if p=="year":   df[name]=df[v].dt.year
         if p=="month":  df[name]=df[v].dt.month
         if p=="week":   df[name]=df[v].dt.week
-        if p=="day":    df[name]=df[v].dt.dayofweek
-        print(df[name].value_counts(normalize=True)) # retain day of week
+        if p=="day":    df[name]=(df[v].dt.dayofweek)+1 # +1 to make the week start (more readable) with day 1 (instead of 0)
+        # print(df[name].value_counts(normalize=True)) # retain day of week at launch
 
-        
-# webrobots data frame contained outliers before 2009 (launch of kickstarter platform)
-df.launch_year.value_counts() # but: outliers before 2009 already dropped with reduction to finished projects
-print(pd.crosstab(df.launch_year,df.state,normalize="index"))
+## date-related distributions   
+# year        
+# original webrobots data frame contained outliers before 2009 (launch of kickstarter platform)
+df.launched_year.value_counts() # but: outliers before 2009 already dropped with reduction to finished projects
+print(pd.crosstab(df.launched_year,df.status,normalize="index"))
+sns.displot(x=df.launched_year,hue=df.status)
+plt.xlabel("year a campaign was launched")
+plt.title("Distribution of campaign launches over years by status");
 
-# derive duration of a campaign in different units
-df['duration_s']=(df['end']-df['launch'])/timedelta(seconds=1)
-df['duration_min']=(df['end']-df['launch'])/timedelta(minutes=1)
-df['duration_h']=(df['end']-df['launch'])/timedelta(hours=1)
-df['duration_d']=(df['end']-df['launch'])/timedelta(days=1)
-df['duration_mon']=df['duration_s']/86400/30 # derive duration in months
-df['duration_y']=df['duration_s']/86400/30/12 # derive duration in years
-df[['duration_h','duration_d','duration_mon','duration_y']].describe() # duration_h and duration_d should be sufficient -> drop else
-df.info()
+# month of launch     
+sns.displot(x=df.launched_month,hue=df.status)
+plt.xlabel("month a campaign was launched")
+plt.title("Distribution of campaign launches over months by status");
+
+# weekday        
+sns.displot(x=df.launched_day,hue=df.status)
+plt.xlabel("weekday a campaign was launched")
+plt.title("Distribution of campaign launches over weekdays by status");
+
+# duration 
+df.duration.value_counts()
+sns.displot(df.duration,kde=True)
+plt.ylim(0,10000);
+plt.xlabel("duration in days")
+plt.title("Distribution of campaign durations");
 
 
 ## Main and sub categories
+df.describe(include="object")
+
+# reverse column names of category variables
+new={"main_category":"sub_category",
+"sub_category":"main_category"}
+df.rename(columns=new,inplace=True)
+
 # main category
-pd.DataFrame(df.main_category.value_counts(normalize=True))
+pd.DataFrame(df.main_category.value_counts(normalize=True)[:10])
 df.main_category.value_counts()
+
+# sub category
+pd.DataFrame(df.sub_category.value_counts(normalize=True))
+df.sub_category.value_counts()
 
 # What are the contents (sub categories) of each main category?
 for cat in df.main_category.value_counts().index.tolist():
@@ -95,62 +124,68 @@ df.category.value_counts()[:10]
 df.category.nunique() # 159 modalities are way too many
 
 
+## backers
+# sns.displot(x=df.backers_count,hue=df.status)
+# plt.xlabel("number of backers a campaign attracted")
+# plt.title("Distribution of backers by status");
+
+
+## goal
+# sns.displot(x=df.goal_usd,hue=df.status)
+sns.displot(x=df.goal_usd)
+plt.xlabel("goal height of a campaign")
+# plt.title("Distribution of goal height by status");
+plt.title("Distribution of goal height");
+
+
 
 ## (pre)select features (columns) to retain for further processing
 # define selection
-list_active=["backers",
-             "main_category",
-             "country","usd_goal_real","usd_pledged_real",
-             "launch_year","launch_day",
-             "duration_h","duration_d",
-             "state"]
-# list of maybe-variables
-list_unclear=["end_year","end_day"]
+list_final=['id', 'currency', 'backers_count', 'country', 'status', 'usd_pledged','main_category', 'creator_id', 'goal_usd', 'city', 'launched_year','duration']
+
+# ['id', 'currency', 'backers_count', 'country', 'status', 'usd_pledged','sub_category', 'creator_id', 'goal_usd', 'city', 'launched_year','duration']
+
 
 # reduce dataset to selection
-df_active=df.ID
-df_active=pd.concat([df_active,df[list_active]],axis=1)
-df_active=df_active.set_index("ID")
-df_active.info()
+# df_final=df.id
+# df_final=pd.concat([df_final,df[list_final]],axis=1)
+# df_final=df_final.set_index("id")
+# df_final.info()
 
+df_final=df[list_final]
+df_final.info()
 
 # ------------------------------------------------------------------------------
 
 ### Preprocessing
 
 ## 
-# check for duplicates
-df_active.duplicated().sum() # 0
-df_active.index.duplicated().sum() # 0
 
 # check for missing values
-df_active.isna().sum().sum() # 0
+df_final.isna().sum().sum() # 0
 
 # check for outliers
-df_active.describe().T
-for col in df_active.select_dtypes("number"):
-    print("\n",col,":\n",df_active[col].value_counts())
+df_final.describe().T
+# numeric variables
+for col in df_final.select_dtypes("number"):
+    print("\n",col,":\n",df_final[col].value_counts())
 # no outliers found
-df_active.country.value_counts() # 210 campaigns of country 'N,0"'
-df_active.country.value_counts(normalize=True) # <0.1 % -> drop
-df_active=df_active.loc[df_active["country"]!='N,0"']
 
-# however, duration_d may be recoded (starting with 1 instead of 0)
-df_active["launch_day"]=df_active["launch_day"]+1
-df_active["launch_day"].value_counts()
-
+# categorical variable country
+df_final.country.value_counts() # no outliers
+df_final.country.value_counts(normalize=True) #
 
 
 
 ## standardization (since variables don't share same scale)
 # first of all: split into train and test set
 from sklearn.model_selection import train_test_split
-X=df_active.drop("state",axis=1)
-y=df_active.state
+X=df_final.drop("status",axis=1)
+y=df_final.status
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.3)
 
 # standardization
-df_active.info()
+df_final.info()
 from sklearn.preprocessing import StandardScaler
 scaler=StandardScaler()
 # select variables to scale - date variables should not be scaled
@@ -165,10 +200,10 @@ X_train.isna().sum()
 # encode explanatory variables
 
 # main category
-df_active.main_category.value_counts() # simply OHE
+df_final.main_category.value_counts() # simply OHE
 
 # country
-df_active.country.value_counts() # divide into US, GB/CA, rest
+df_final.country.value_counts() # divide into US, GB/CA, rest
 
 # encode in a loop over train and test set
 for s in [X_train,X_test]:
@@ -192,7 +227,7 @@ for s in [X_train,X_test]:
 # encode target variable
 for s in [y_train,y_test]:
     # main category
-    s=pd.get_dummies(s,columns=["state"],prefix="",prefix_sep="")
+    s=pd.get_dummies(s,columns=["status"],prefix="",prefix_sep="")
     print(s.value_counts())
     print(s.isna().sum().sum())
 
@@ -203,26 +238,26 @@ for s in [y_train,y_test]:
 ### Visualization
 
 ## BEFORE preprocessing
+cols=['backers_count','usd_pledged','goal_usd','launched_year','duration']
 # (no?) correlations betwwen explanatory variables
-sns.heatmap(df_active.corr(),cmap='winter',annot=True)
-plt.title('Correlation of all variables');
-cols=['backers','usd_pledged_real','usd_goal_real','duration_h','duration_d']
-sns.pairplot(df_active[cols],diag_kind='kde')
-# sns.pairplot(df_active,diag_kind='kde')
-plt.title('Relation of subset of variables');
+sns.heatmap(df_final[cols].corr(),cmap='winter',annot=True)
+plt.title('Correlation of variables');
+
+sns.pairplot(df_final[cols],diag_kind='kde')
+plt.title('Distribution of variables');
 
 # relation between backers and converted pledged amount
-plt.scatter(df_active['backers'],df_active['usd_pledged_real'])
-plt.plot((df_active["backers"].min(),df_active["backers"].max()),(df_active["usd_pledged_real"].min(),                                                             df_active["usd_pledged_real"].max()),"red")
-plt.xlabel('backers count')
-plt.ylabel('(converted) pledged amount')
+plt.scatter(df_final['backers_count'],df_final['usd_pledged'])
+plt.plot((df_final["backers_count"].min(),df_final["backers_count"].max()),(df_final["usd_pledged"].min(),                                                             df_final["usd_pledged"].max()),"red")
+plt.xlabel('number of backers')
+plt.ylabel('pledged amount')
 plt.title('backers and pledged amount');
 
 
-# Development of funding goals by project (top 3 of main) category and state over time
+# Development of funding goals by project (top 3 of main) category and status over time
 select=["Comics","Technology","Film & Video"]
-g=sns.catplot(x="launched_year",y="backers_count",hue="state",row="cat_parent_name",\
-            row_order=select,col="state",kind="bar",errorbar=('ci',False),height=4,data=df_active);
+g=sns.catplot(x="launched_year",y="backers_count",hue="status",row="cat_parent_name",\
+            row_order=select,col="status",kind="bar",errorbar=('ci',False),height=4,data=df_final);
 g.set_axis_labels("Year of project launch","Number of backers")
 plt.tick_params(bottom='off',labelbottom='on')
 # plt.xticks(rotation=30) # does it only for the very last subplot
@@ -232,20 +267,15 @@ g.fig.subplots_adjust(top=.93)
 # modify titles of subplots
 g.set_titles("{row_name} ({col_name})")
 # add general grid title
-g.fig.suptitle("Development of backers' quantity by project (top 3 of main) category and state over time");
+g.fig.suptitle("Development of backers' quantity by project (top 3 of main) category and status over time");
 
 
-# All the currency values are converted into appropriate amounts in USD as per the fx rate provided in the base dataset.
-# --> we should use the _real columns for pledged and goal to be able to compare amounts across countries
-df.columns
-cols=['backers','usd_pledged_real','usd_goal_real','duration_h','duration_d',]
-df[cols].head(10)
-df[cols].describe()
-for col in cols:
-    sns.displot(df[col],kde=True);
-plt.show()
-    
-df[["usd_goal_real","usd_pledged_real"]].head(10)
-df[["usd_goal_real","usd_pledged_real"]].describe()
-df["usd_goal_real"].value_counts()[:10]
-df["usd_pledged_real"].value_counts()[:10]
+# ANOVA
+num_list=['backers_count',
+       'usd_pledged','goal_usd',
+       'launched_year','duration']
+import statsmodels.api
+for item in num_list:
+    print('###',item)
+    result=statsmodels.formula.api.ols(f'{item} ~ status',data=df_final).fit()
+    display(statsmodels.api.stats.anova_lm(result))
